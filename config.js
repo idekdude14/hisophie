89bf2871223cd490058dc2c2a5ec333a56491fb7
idekdude14 <jes.rioflorido@gmail.com>
2026-02-04 10:30:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -72,7 +72,15 @@ const CONFIG = {
         bounceSpeed: "0.5s",            // Speed of bouncing animations (0.3-0.7s recommended)
         heartExplosionSize: 1.5         // Size of heart explosion effect (1.2-2.0 recommended)
     },
-
+    
+    music: {
+        enabled: true, // Music feature is enabled
+        autoplay: true, // Try to autoplay (note: some browsers may block this)
+        musicUrl: "https://res.cloudinary.com/dfmbgw8gk/video/upload/v1770172077/videoplayback_wtxd1p.mp4", // Paste your music URL here
+        startText: "🎵 Play Music", // Button text to start music
+        stopText: "🔇 Stop Music", // Button text to stop music
+        volume: 0.5 // Volume level (0.0 to 1.0)
+    
 };
 
 // Don't modify anything below this line unless you know what you're doing
