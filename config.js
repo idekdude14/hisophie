e23000582e5a83b79c84935cf18eaf867a16c6ac
idekdude14 <jes.rioflorido@gmail.com>
2026-02-04 10:14:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://www.youtube.com/watch?v=c-NjTztPLXI", // Music streaming URL
+        musicUrl: "https://www.epidemicsound.com/music/tracks/7e3d79e1-9202-4b22-859b-5a8e408fb8a3/", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
